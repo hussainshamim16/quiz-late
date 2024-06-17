ae55e3c2d39762bfc085e70cfdfb9da8252d0066
MUHAMMAD HUSSAIN SHAMIM <hj422748@gmail.com>
2024-06-17 09:06:11 +0500

    the script file

--- moreFolders/user.js
+++ moreFolders/user.js
@@ -1,5 +1,6 @@
        // student form data 
-       var atag = document.getElementById("atag")
+       var form = document.getElementsByTagName("form")[0]
+    //    console.log(form.style.background= "red")
        function stdSumbit(element) {
         if (!stdName.value) {
             console.log("error")
@@ -14,7 +15,7 @@
             rollNumbererror.className = "show"
             return
         } else {
-            atag.href="user.html"
+            
             rollNumbererror.className = "hide";
             stdLogin.style.display = "none"
             // startbtn.style.display = "block"
@@ -25,5 +26,6 @@
             localStorage.setItem("student Roll Number", stdRollNumber.value)
                 stdName.value = ""
                 stdRollNumber.value = ""
+                form.action ="/index.html"
         }
     }
